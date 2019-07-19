c15b8fdb4acad6b206a5a3a23c8a5d66855a16ed
mts513 <34582160+mts513@users.noreply.github.com>
2019-07-18 18:10:12 -0700

    added drop down list from keyword array

--- js/app.js
+++ js/app.js
@@ -1,49 +1,59 @@
-'use strict';
+"use strict";
 
-function Creature(animal){
-    this.keyword = animal.keyword;
-    this.image_url = animal.image_url;
-    this.title = animal.title;
-    this.description = animal.description;
-    this.horns = animal.horns;
+function Creature(animal) {
+  this.keyword = animal.keyword;
+  this.image_url = animal.image_url;
+  this.title = animal.title;
+  this.description = animal.description;
+  this.horns = animal.horns;
 }
 Creature.allCreatures = [];
 
 Creature.prototype.render = function() {
-    $('main').append('<div class="clone"></div>');
-    let creatureClone = $('div[class="clone"]');
-    let creatureHtml = $('#photo-template').html();
-    creatureClone.html(creatureHtml);
-    creatureClone.find('h2').text(this.title);
-    creatureClone.find('img').attr('src', this.image_url).attr('alt', "alt text");
-    creatureClone.find('p').text(this.description);
-    creatureClone.removeClass('clone');
-    creatureClone.attr('class', this.horns);
+  $("main").append('<div class="clone"></div>');
+  let creatureClone = $('div[class="clone"]');
+  let creatureHtml = $("#photo-template").html();
+  creatureClone.html(creatureHtml);
+  creatureClone.find("h2").text(this.title);
+  creatureClone
+    .find("img")
+    .attr("src", this.image_url)
+    .attr("alt", this.description);
+  creatureClone.find("p").text(this.description);
+  creatureClone.removeClass("clone");
+  creatureClone.attr("class", this.horns);
 };
 
 Creature.readJson = () => {
-    $.get('../data/page-1.json', 'json')
+  $.get("../data/page-1.json", "json")
     .then(data => {
-        data.forEach(item => {
-            Creature.allCreatures.push(new Creature(item));            
-        });
+      data.forEach(item => {
+        Creature.allCreatures.push(new Creature(item));
+      });
     })
     .then(Creature.loadCreatures);
 };
 
 Creature.loadCreatures = () => {
-    Creature.allCreatures.forEach(item => item.render());
+  Creature.allCreatures.forEach(item => item.render());
+  Creature.makeOption();
 };
 
 $(() => Creature.readJson());
-
+// Below is JS for creating list options
 Creature.SelectOptions = [];
 
-Creature.makeOption = function(){
-    let SelectOptionsClone = this.SelectOptions;
-Creature.allCreatures.forEach(function(critter){
+Creature.makeOption = function() {
+  let SelectOptionsClone = this.SelectOptions;
+  Creature.allCreatures.forEach(function(critter) {
     if ($.inArray(critter.keyword, SelectOptionsClone) === -1) {
-        SelectOptionsClone.push(critter.keyword);
+      SelectOptionsClone.push(critter.keyword);
     }
-}
-)}
+  });
+  for (var i = 0; i < SelectOptionsClone.length; i++) {
+    $("select").append(
+      "<option value=" + i + ">" + SelectOptionsClone[i] + "</option>"
+    );
+  }
+};
+// Below is updating the list on site
